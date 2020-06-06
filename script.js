d00215c8be70b65a240be638f8bdca3dedac6c75
Eunah Kim <Eunahkim92@gmail.com>
2020-06-05 23:49:43 -0400

    Refactor code to use es6

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 generatePassword = () => {
   // Grab data from user
-  var characterLength = prompt("How many characters would you like your password to contain?");
-  // Convert user input (string) into number
-  var lengthInt = parseInt(characterLength);
-  // console.log(lengthInt);
+  const characterLength = parseInt(prompt("How many characters would you like your password to contain?"));
 
-  var userArray = [];
-  var password = '';
+  let userArray = [];
+  let password = '';
 
   // Note: total possible choices: 94
   var lowercaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var uppercaseArr = [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];]
   var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var specialArr = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\'', ']', '^', '_', '`', '{', '|', '}', '~'];
 
@@ -33,25 +30,21 @@ generatePassword = () => {
       alert("Your password can only contain up to 128 characters.");
       return '';
   } else if (lengthInt >= 8 || lengthInt <= 128) {
-      var lowerCase = confirm("Click OK if you would like lowercase letters in your password.");
+      const lowerCase = confirm("Click OK if you would like lowercase letters in your password.");
         if (lowerCase === true) {
-          var userArray = userArray.concat(lowercaseArr);
-          // console.log(userArray);
+          const userArray = userArray.concat(lowercaseArr);
         } 
-      var upperCase = confirm("Click OK if you would like uppercase letters in your password.");
+      const upperCase = confirm("Click OK if you would like uppercase letters in your password.");
         if (upperCase === true) {
-          var userArray = userArray.concat(uppercaseArr);
-          // console.log(userArray);
+          const userArray = userArray.concat(uppercaseArr);
         } 
-      var numbers = confirm("Click OK if you would like numbers in your password.");
+      const numbers = confirm("Click OK if you would like numbers in your password.");
         if (numbers === true) {
-          var userArray = userArray.concat(numberArr);
-          // console.log(userArray);
+          const userArray = userArray.concat(numberArr);
         } 
-      var specialCharacters = confirm("Click OK if you would like special characters in your password.");
+      const specialCharacters = confirm("Click OK if you would like special characters in your password.");
         if (specialCharacters === true) {
-          var userArray = userArray.concat(specialArr);
-          // console.log(userArray);
+          const userArray = userArray.concat(specialArr);
         } 
       // Check to see if user chose at least one criteria
       if (lowerCase === false && upperCase === false && numbers === false && specialCharacters === false) {
@@ -61,8 +54,8 @@ generatePassword = () => {
       return '';
   }
 
-  for (var i = 0; i < lengthInt; i++) {
-    var num = Math.floor(Math.random() * userArray.length);
+  for (let i = 0; i < lengthInt; i++) {
+    let num = Math.floor(Math.random() * userArray.length);
     password += userArray[num] + "";
   }
   // console.log(password);
