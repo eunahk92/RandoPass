e5aa6080947c6408e21e39f96ecaacdde6138dc2
Eunah Kim <Eunahkim92@gmail.com>
2020-06-06 00:00:14 -0400

    Refactor code to utilize es6

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  const password = generatePassword();
+  let password = generatePassword();
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,16 +11,17 @@ function writePassword() {
 
 generatePassword = () => {
   // Grab data from user
-  const characterLength = parseInt(prompt("How many characters would you like your password to contain?"));
+  let characterLength = prompt("How many characters would you like your password to contain?");
+  let lengthInt = parseInt(characterLength);
 
   let userArray = [];
   let password = '';
 
   // Note: total possible choices: 94
-  var lowercaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var specialArr = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\'', ']', '^', '_', '`', '{', '|', '}', '~'];
+  const lowercaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const specialArr = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\'', ']', '^', '_', '`', '{', '|', '}', '~'];
 
   // Check if user input fits criteria
   if (lengthInt < 8) {
@@ -30,25 +31,26 @@ generatePassword = () => {
       alert("Your password can only contain up to 128 characters.");
       return '';
   } else if (lengthInt >= 8 || lengthInt <= 128) {
-      const lowerCase = confirm("Click OK if you would like lowercase letters in your password.");
-        if (lowerCase === true) {
-          const userArray = userArray.concat(lowercaseArr);
+      let lowerCase = confirm("Click OK if you would like lowercase letters in your password.");
+        if (lowerCase) {
+          userArray = userArray.concat(lowercaseArr);
         } 
-      const upperCase = confirm("Click OK if you would like uppercase letters in your password.");
-        if (upperCase === true) {
-          const userArray = userArray.concat(uppercaseArr);
+      let upperCase = confirm("Click OK if you would like uppercase letters in your password.");
+        if (upperCase) {
+          userArray = userArray.concat(uppercaseArr);
         } 
-      const numbers = confirm("Click OK if you would like numbers in your password.");
-        if (numbers === true) {
-          const userArray = userArray.concat(numberArr);
+      let numbers = confirm("Click OK if you would like numbers in your password.");
+        if (numbers) {
+          userArray = userArray.concat(numberArr);
         } 
-      const specialCharacters = confirm("Click OK if you would like special characters in your password.");
-        if (specialCharacters === true) {
-          const userArray = userArray.concat(specialArr);
+      let specialCharacters = confirm("Click OK if you would like special characters in your password.");
+        if (specialCharacters) {
+          userArray = userArray.concat(specialArr);
         } 
       // Check to see if user chose at least one criteria
-      if (lowerCase === false && upperCase === false && numbers === false && specialCharacters === false) {
-        alert("Your password must contain at least one property in order to generate. Please try again.")
+      if (!lowerCase && !upperCase && !numbers && !specialCharacters) {
+        alert("Your password must contain at least one property in order to generate. Please try again.");
+        return '';
       }
   } else {
       return '';
